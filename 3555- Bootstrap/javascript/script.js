3865e221738cfc8d533926d9bee612b98bd4b0fa
SharmaRahul321 <rahulberojya06082002@gmail.com>
2024-01-31 11:06:09 +0530

    change bootstrap code

---
+++ 3555- Bootstrap/javascript/script.js
@@ -0,0 +1,21 @@
+
+function toggleMode() {
+    var body = document.body;
+    body.classList.toggle('dark-mode');
+
+    // Get all elements with the class 'card'
+    var cards = document.querySelectorAll('.card');
+
+    // Loop through each card and update background and text color based on the theme
+    cards.forEach(function (card) {
+        if (body.classList.contains('dark-mode')) {
+            // Dark theme
+            card.style.backgroundColor = '#343a40'; // Dark background color
+            card.style.color = '#00f'; // Blue text color
+        } else {
+            // Light theme
+            card.style.backgroundColor = ''; // Reset background color
+            card.style.color = '#000'; // Black text color
+        }
+    });
+}
